324faae6d7a9753590e7d42a9219e85f7e04990e
Nandhini Kirubakaran <knaphasri@gmail.com>
2023-01-14 17:37:20 +0530

    vercel url add

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 // export const API = "https://638cdc36eafd555746b0d43f.mockapi.io";
-export const API = "http://localhost:5000";
+// export const API = "http://localhost:5000";
+export const API = "https://expressjs-green.vercel.app";
